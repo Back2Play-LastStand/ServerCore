3c77dd0c01e2491d1507e60c4c634afe251e7c32
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-05 18:22:01 +0900

    base setting 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+#include "socket.h"
+using namespace cppx;
+
 int main()
 {
+	native::init(5);
+	cppx::socket sock(protocol::tcp);
+	if (!sock.not_invalid())
+		return -1;
+	if (!sock.bind(endpoint(ip_address::any, 7777)))
+		return -1;
+	if (!sock.listen())
+		return -1;
+
+	while (true)
+	{
+
+	}
+
 	return 0;
 }

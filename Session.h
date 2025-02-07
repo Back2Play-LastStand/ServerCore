2f712ce991b80df7a81bbe90fbe5202b4febcc04
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-07 19:56:35 +0900

    member variable

--- Session.h
+++ Session.h
@@ -1,9 +1,18 @@
 #pragma once
+using namespace std;
+#include <vector>
+#include "context.h"
 
 class Session
 {
 public:
 	Session();
 	virtual ~Session();
+
+private:
+	shared_ptr<cppx::socket> m_sock;
+	vector<char> m_buffer;
+	cppx::context m_recvContext;
+	cppx::context m_sendContext;
 };
 

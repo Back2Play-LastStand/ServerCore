a3c5e05b81d0323411757ab5ff4341a5b004f66d
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-10 21:01:56 +0900

    server run(endpoint, int)

--- cpp/Network/Server.cpp
+++ cpp/Network/Server.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "Network\Server.h"
+#include "Network/Session.h"
 
 Server::Server()
 {
@@ -7,5 +8,34 @@ Server::Server()
 }
 
 Server::~Server()
+{
+}
+
+void Server::Run(endpoint ep, int count)
+{
+	if (!m_listenSocket.bind(endpoint(ep)))
+	{
+		cout << "bind error" << endl;
+		return;
+	}
+	if (!m_listenSocket.listen())
+	{
+		cout << "listen error" << endl;
+		return;
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		auto acceptContext = new context;
+		acceptContext->_socket = make_shared<cppx::socket>(protocol::tcp);
+		
+		if (!m_listenSocket.accept(acceptContext))
+			AcceptCompleted(acceptContext);
+
+		m_acceptContext.emplace_back(acceptContext);
+	}
+}
+
+int Server::AcceptCompleted(context* acceptContext)
 {
 }
--- h/Network/Server.h
+++ h/Network/Server.h
@@ -6,7 +6,11 @@ public:
 	Server();
 	virtual ~Server();
 
+	void Run(endpoint ep, int count);
+	int AcceptCompleted(context* acceptContext);
+
 private:
 	cppx::socket m_listenSocket;
+	vector<context> m_acceptContext;
 };
 

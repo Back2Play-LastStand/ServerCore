181e1fc2273dc39c58e973612bf6fe097d4e7a79
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-12 23:36:13 +0900

    create StompAllocator

---
+++ cpp/Allocator/Allocator.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "Memory/Allocator.h"
+
+void* StompAllocator::Alloc(int size)
+{
+	const long long pageCount = (size + PAGE_SIZE - 1) / PAGE_SIZE;
+	const long long dataOffset = pageCount * PAGE_SIZE - size;
+	void* baseAddress = ::VirtualAlloc(NULL, pageCount + PAGE_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	return static_cast<void*>(static_cast<char*>(baseAddress) + dataOffset);
+}
+
+void StompAllocator::Release(void* ptr)
+{
+	const long long address = reinterpret_cast<long long>(ptr);
+	const long long baseAddress = address - (address % PAGE_SIZE);
+	::VirtualFree(reinterpret_cast<void*>(baseAddress), 0, MEM_RELEASE);
+}
---
+++ h/Memory/Allocator.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class StompAllocator
+{
+	enum { PAGE_SIZE = 0x1000 };
+
+public:
+	static void* Alloc(int size);
+	static void Release(void* ptr);
+};

059c2710e992ef1cf4ab0c3b001dbb52235577be
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-12 23:49:18 +0900

    Memory

---
+++ cpp/Memory/Memory.cpp
@@ -0,0 +1,96 @@
+#include "pch.h"
+#include "Memory.h"
+#include "Memory/MemoryPool.h"
+
+Memory::Memory()
+{
+	int size = 0;
+	int tableIndex = 0;
+
+	for (size = 32; size <= 1024; size += 32)
+	{
+		MemoryPool* pool = new MemoryPool(size);
+		_pools.push_back(pool);
+
+		while (tableIndex <= size)
+		{
+			_poolTable[tableIndex] = pool;
+			tableIndex++;
+		}
+	}
+
+	for (; size <= 2048; size += 128)
+	{
+		MemoryPool* pool = new MemoryPool(size);
+		_pools.push_back(pool);
+
+		while (tableIndex <= size)
+		{
+			_poolTable[tableIndex] = pool;
+			tableIndex++;
+		}
+	}
+
+	for (; size <= 4096; size += 256)
+	{
+		MemoryPool* pool = new MemoryPool(size);
+		_pools.push_back(pool);
+
+		while (tableIndex <= size)
+		{
+			_poolTable[tableIndex] = pool;
+			tableIndex++;
+		}
+	}
+}
+
+Memory::~Memory()
+{
+	for (MemoryPool* pool : _pools)
+		delete pool;
+
+	_pools.clear();
+}
+
+void* Memory::Allocate(int size)
+{
+	MemoryHeader* header = nullptr;
+	const int allocSize = size + sizeof(MemoryHeader);
+
+#ifdef _STOMP
+	header = reinterpret_cast<MemoryHeader*>(StompAllocator::Alloc(allocSize));
+#else
+	if (allocSize > MAX_ALLOC_SIZE)
+	{
+		header = reinterpret_cast<MemoryHeader*>(::_aligned_malloc(allocSize, SLIST_ALIGNMENT));
+	}
+	else
+	{
+		header = _poolTable[allocSize]->Pop();
+	}
+#endif
+
+	return MemoryHeader::AttachHeader(header, allocSize);
+}
+
+void Memory::Release(void* ptr)
+{
+	MemoryHeader* header = MemoryHeader::DetachHeader(ptr);
+
+	const int allocSize = header->allocSize;
+	if (allocSize <= 0)
+		return;
+
+#ifdef _STOMP
+	StompAllocator::Release(header);
+#else
+	if (allocSize > MAX_ALLOC_SIZE)
+	{
+		::_aligned_free(header);
+	}
+	else
+	{
+		_poolTable[allocSize]->Push(header);
+	}
+#endif
+}

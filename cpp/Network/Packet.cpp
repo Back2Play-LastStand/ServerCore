02a39698c6f4e1c49c860fda4def4a649c7d37b7
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-13 23:08:11 +0900

    Packet Set(PacketInfo&) (uint, PacketHeader, char*)

--- cpp/Network/Packet.cpp
+++ cpp/Network/Packet.cpp
@@ -9,3 +9,19 @@ vector<char> Packet::GetData()
 {
 	return m_buffer;
 }
+
+void Packet::Set(PacketInfo& packet)
+{
+	m_packetInfo.clientIndex = packet.clientIndex;
+	m_packetInfo.packetHeader = packet.packetHeader;
+	m_packetInfo.data = new char[packet.packetHeader.size];
+	CopyMemory(m_packetInfo.data, packet.data, packet.packetHeader.size);
+}
+
+void Packet::Set(unsigned int clientIndex, PacketHeader packetHeader, char* data)
+{
+	m_packetInfo.clientIndex = clientIndex;
+	m_packetInfo.packetHeader = packetHeader;
+	m_packetInfo.data = new char[packetHeader.size];
+	CopyMemory(m_packetInfo.data, data, packetHeader.size);
+}

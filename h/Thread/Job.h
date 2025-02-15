024dca59970abff30b45a0c2864c3d19232bf791
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-15 16:53:33 +0900

    Job constructor

--- h/Thread/Job.h
+++ h/Thread/Job.h
@@ -1,7 +1,26 @@
 #pragma once
 
+using CallbackJob = function<void()>;
+
 class Job
 {
+public:
+	Job(CallbackJob&& callback) : m_callback(move(callback))
+	{
+	}
+
+	template<typename T, typename Ret, typename... Args>
+	Job(shared_ptr<T> owner, Ret(T::* memFunc)(Args...), Args&&... args)
+	{
+		m_callback = [owner, memFunc, args...]()
+		{
+			(owner.get()->*memFunc)(args...);
+		}
+	}
+
+
+private:
+	CallbackJob m_callback;
 };
 
 class JobSerializer : public enable_shared_from_this<JobSerializer>

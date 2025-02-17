a2f4f6d6ac752a28bc4d32d902c7590041ab4d97
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-17 21:02:41 +0900

    create getter

--- h/System/Engine.h
+++ h/System/Engine.h
@@ -6,6 +6,9 @@ public:
 	Engine();
 	~Engine();
 
+	inline ThreadManager* GetThreadManager() { return m_threadManager; }
+	inline JobTimer* GetJobTimer() { return m_jobTimer; }
+
 private:
 	ThreadManager* m_threadManager = nullptr;
 	JobTimer* m_jobTimer = nullptr;

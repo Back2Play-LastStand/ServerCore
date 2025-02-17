cc2d755a72bdccad2b784cbd39d11e15528053f7
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-17 15:10:59 +0900

    create JobData

--- h/Thread/Job.h
+++ h/Thread/Job.h
@@ -49,3 +49,21 @@ private:
 	concurrent_queue<shared_ptr<Job>> m_jobs;
 	atomic<int> m_jobCount = 0;
 };
+
+struct JobData
+{
+	JobData() = default;
+	JobData(long long executeTick, weak_ptr<JobSerializer> owner, shared_ptr<Job> job) : executeTick(executeTick), owner(owner), job(job)
+	{
+	}
+
+	bool operator<(const JobData& other) const
+	{
+		return executeTick < other.executeTick;
+	}
+
+	long long executeTick = 0;
+	weak_ptr<JobSerializer> owner;
+	shared_ptr<Job> job;
+};
+};

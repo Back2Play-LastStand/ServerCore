3e54af42c2837ddfed389e0a2a1fee41aa9826c5
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-19 13:57:25 +0900

    Engine Run Thread, IO

--- h/System/Engine.h
+++ h/System/Engine.h
@@ -14,11 +14,18 @@ public:
 	ThreadManager* GetThreadManager();
 	JobTimer* GetJobTimer();
 
+	void RunThread(int io, int worker);
+	void GlobalQueueWork();
+
+private:
+	void RunIo(int threadCount);
+	void RunQueueWorker(int threadCount);
+
 private:
 	ThreadManager* m_threadManager = nullptr;
 	JobTimer* m_jobTimer = nullptr;
 
-	concurrent_queue<JobSerializer> m_jobSerializerQueue;
+	concurrent_queue<JobSerializer*> m_jobSerializerQueue;
 };
 
 extern Engine* GEngine;

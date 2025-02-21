7957a3c339c11c2ba03c23a96d90fd3d9c3e653b
hyuli0520 <minhyeogjeon945@gmail.com>
2025-02-21 10:49:02 +0900

    make GetTable(string)

--- h/Database/DBConnection.h
+++ h/Database/DBConnection.h
@@ -13,6 +13,20 @@ public:
 
 	bool Execute();
 
+	mysqlx::Table GetTable(string schemaName)
+	{
+		try
+		{
+			auto table = m_schema->getTable(schemaName);
+			cout << "Successful get table " << schemaName << endl;
+			return table;
+		}
+		catch (const mysqlx::Error& err)
+		{
+			cout << "An error occured : " << err.what() << endl;
+		}
+	}
+
 private:
 	unique_ptr<mysqlx::Schema> m_schema;
 	unique_ptr<mysqlx::Session> m_session;
